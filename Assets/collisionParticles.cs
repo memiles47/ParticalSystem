fd2341209d90f25a2e0930abd7aa8148c26c0486
Michael Miles <boat081062@gmail.com>
2015-10-24 12:58:46 -0500

    Using Sprites

---
+++ Assets/collisionParticles.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionParticles : MonoBehaviour {
+
+void OnParticleCollision(GameObject other)
+	{
+		Destroy (other.gameObject);
+	}
+
+}
